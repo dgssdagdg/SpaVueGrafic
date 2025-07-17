28ee1b86ac32474aa565cb7a165f84e346db472f
Valid Magomadov <100527555+dgssdagdg@users.noreply.github.com>
2025-07-17 22:26:20 +0300

    Снова изменения

--- src/composables/useFetchData.js
+++ src/composables/useFetchData.js
@@ -1,16 +1,15 @@
-const API_HOST = 'http://109.73.206.144:6969';
+const API_HOST = '/api'; // Используем относительный путь
 const API_KEY = 'E6kUTYrYwZq2tN4QEtyzsbEBk3ie';
 
 export default async function useFetchData(endpoint, dateFrom, dateTo, page, limit = 25) {
   try {
     const url = new URL(`${API_HOST}${endpoint}`);
     url.searchParams.append('dateFrom', dateFrom);
-    if(dateTo){ url.searchParams.append('dateTo', dateTo)}
+    if(dateTo) url.searchParams.append('dateTo', dateTo);
     url.searchParams.append('page', page);
     url.searchParams.append('limit', limit);
     url.searchParams.append('key', API_KEY);
 
-    console.log(url.toString());
     const response = await fetch(url.toString());
     if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
     
